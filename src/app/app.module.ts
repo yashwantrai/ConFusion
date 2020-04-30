5a5c5a16f24ca3d8f546f2df126dc008cda085d0
yashwant rai <yashwant.rai@gmail.com>
2020-04-29 17:10:43 -0700

    angular component part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {FlexLayoutModule } from '@angular/flex-layout';
-
-
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatListModule} from '@angular/material/list'
@@ -22,7 +23,10 @@ import { MenuComponent } from './menu/menu.component'
     AppRoutingModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule
 
   ],
   providers: [],
