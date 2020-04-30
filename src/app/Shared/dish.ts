3970c5861df96d2b227cd380569f4ae847f3f197
yashwant rai <yashwant.rai@gmail.com>
2020-04-29 22:50:29 -0700

    adding the dishes class

--- src/app/Shared/dish.ts
+++ src/app/Shared/dish.ts
@@ -1,3 +1,4 @@
+import {Comment} from './comment'
 export class Dish{
     id: string; 
     name: string;
@@ -7,6 +8,6 @@ export class Dish{
     label: string;
     price: string;
     description: string;
-
+    comments: Comment[];
 
 }
